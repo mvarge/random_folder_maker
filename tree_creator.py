3cdd1835de9d94dc8e28827fbe8fb94b82c808f8
mvarge <marcelo.varge@gmail.com>
2018-01-20 01:04:37 +0000

    adding functions

--- tree_creator.py
+++ tree_creator.py
@@ -1,7 +1,34 @@
+import os
 import random
 from words import words
+
+
+class Tree(object):
+
+    def __init__(self, nodes=1, max_depth=1):
+        self.nodes = xrange(nodes)
+        self.max_depth = max_depth
+
+    def depth(self):
+        return random.randint(0, self.max_depth)
+
+    def gen_tree(self):
+        os.makedirs("fake_tree")
+        os.chdir(os.path.join(os.getcwd(), "fake_tree"))
+        for node in self.nodes:
+            parent = os.path.join(os.getcwd(), fake_word())
+            os.makedirs(parent)
+            depth = self.depth()
+            while depth:
+                folder = fake_word()
+                os.makedirs(folder)
+                os.chdir(os.path.join(os.getcwd(), folder))
+                depth -= 1
+            os.chdir(parent)
 
 def fake_word():
     return random.choice(words)
 
-
+if __name__ == "__main__":
+    t = Tree(10, 10)
+    t.gen_tree()
