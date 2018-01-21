10908211fa39e05d83bf0dd31b5ff5f376297702
mvarge <marcelo.varge@gmail.com>
2018-01-21 00:26:29 +0000

    adding function to create files

--- helper.py
+++ helper.py
@@ -2,10 +2,12 @@ import argparse
 
 def parse():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-f", "--folders", help="Number of parent folders.", 
+    parser.add_argument("-p", "--folders", help="Number of parent folders.", 
             type=int, default=10)
     parser.add_argument("-d", "--depth", help="Max depth of sub folders. The ammount will \
             be randomly chosen.", type=int, default=10)
     parser.add_argument("-n", "--name", help="Name of parent dir. (Default = 'root')",
             default="root") 
+    parser.add_argument("-f", "--files", help="File numbers to create, if any.",
+            default=0, type=int) 
     return parser.parse_args()
--- tree_creator.py
+++ tree_creator.py
@@ -5,9 +5,10 @@ from words import words
 
 class Tree(object):
 
-    def __init__(self, nodes=1, max_depth=1):
+    def __init__(self, nodes=1, max_depth=1, files=0):
         self.nodes = xrange(nodes)
         self.max_depth = max_depth
+        self.files = files
 
     def depth(self):
         return random.randint(0, self.max_depth)
@@ -34,6 +35,9 @@ class Tree(object):
             os.chdir(os.path.join(parent, word))
             depth = self.depth()
             while depth:
+                for f in xrange(random.randint(0, self.files)):
+                    filename = "{0}.txt".format(self.word())
+                    open(filename, 'a').close()
                 folder = self.word()
                 os.makedirs(folder)
                 os.chdir(os.path.join(os.getcwd(), folder))
@@ -46,5 +50,5 @@ def fake_word():
 if __name__ == "__main__":
     from helper import parse
     args = parse()
-    t = Tree(args.folders, args.depth)
+    t = Tree(args.folders, args.depth, args.files)
     t.gen_tree(args.name)
